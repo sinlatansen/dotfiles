dc12560f7ca7de73f31bdca96dd4c85039b7ec84
lzy <lllzzzyyy@buaa.edu.cn>
2024-03-17 20:17:18 +0800

    新增自动更新脚本，一键更新链接

---
+++ stow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 创建必要的配置目录
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/neofetch
+mkdir -p ~/.config/tmux-powerline
+
+# 切换到 dotfiles 目录
+cd ~/dotfiles
+
+# 使用 stow 链接配置
+stow -R zsh
+stow -R tmux
+stow -R --target=$HOME/.config/nvim/ nvim
+stow -R --target=$HOME/.config/neofetch/ neofetch
+stow -R --target=$HOME/.config/tmux-powerline/ tmux-powerline
+
+echo "Configurations have been stowed."
